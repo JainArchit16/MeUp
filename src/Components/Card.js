0cd5b841bb517d0e4a1a5e92dbeaadb53c843281
Archit <jainarchit1604@gmail.com>
2023-10-03 09:05:49 +0530

    Added Like button

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import {AiOutlineClockCircle} from "react-icons/ai"
+import { FcLike, FcLikePlaceholder } from 'react-icons/fc';
+import { toast } from 'react-hot-toast';
 
 const Card = (props) => {
     let data=props.dataOne;
+    let likedCourses = props.likedCourses;
+    let setLikedCourses = props.setLikedCourses;
+
+    function clickHandler() {
+        // Logic
+        if (likedCourses.includes(props.dataOne.id)) {
+            // pahle se liked 
+            setLikedCourses((prev) => prev.filter((cid) => cid !== props.dataOne.id));
+            toast.error("Liked Removed");
+        }
+        else {
+            // pahle se like nahi hai course 
+            // insert karne h y course like course me 
+            if (likedCourses.length === 0) {
+                setLikedCourses([props.dataOne.id]);
+            }
+            else {
+                setLikedCourses((prev) => [...prev, props.dataOne.id]);
+            }
+            toast.success("Liked Successfully");
+        }
+    }
+
   return (
     
            
@@ -12,6 +37,13 @@ const Card = (props) => {
                 
                     <img src={props.dataOne.image} className='w-full -top-10 h-64'></img>
                     <p className='absolute bg-purple-500  rounded-lg text-white p-1 left-6'>{props.dataOne.tag}</p>
+                    <div className='rounded-full w-[40px] h-[40px] bg-white relative p-1 -right-96 -top-14 bottom-[-12px] grid place-items-center'>
+                    <button onClick={clickHandler}>
+                        {
+                            !likedCourses.includes(props.dataOne.id) ? <FcLikePlaceholder fontSize="1.75rem" /> : <FcLike fontSize="1.75rem" />
+                        }
+                    </button>
+                </div>
                     
                 <p  className='font-bold p-2 pl-10 text-2xl'>{data.title}</p>
                 <div className='flex flex-row items-center opacity-70 pl-10'>
--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -6,20 +6,15 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import { FreeMode, Pagination } from 'swiper/modules';
+import { useState } from 'react';
 
 const Upcoming = (props) => {
+  const [likedCourses, setLikedCourses] = useState([]);
   return (
     <div className='flex flex-col bg-gray-100 p-20 h-[650px]'>
-      <p className='text-3xl font-bold px-3 py-1'>Upcoming Events</p>
-      <p className='text-xl opacity-70 px-3 py-1'>You can choose to display featured</p>
-      <div className='bg-red-500 w-[5%] h-0.5 text-white text-1 mx-3 mt-1 mb-3'> " s"</div>
-      {
-        console.log("Upcomng")
-      }
-      {
-
-      console.log(props.data)
-      }
+        <p className='text-3xl font-bold px-3 py-1'>Upcoming Events</p>
+        <p className='text-xl opacity-70 px-3 py-1'>You can choose to display featured</p>
+        <div className='bg-red-500 w-[5%] h-0.5 text-white text-1 mx-3 mt-1 mb-3'> " s"</div>
       <div>
 
 
@@ -34,7 +29,7 @@ const Upcoming = (props) => {
         className="mySwiper"
       >
         {
-            props.data.map((dataOne) => <SwiperSlide><Card dataOne={dataOne}></Card></SwiperSlide>)
+            props.data.map((dataOne) => <SwiperSlide><Card dataOne={dataOne} likedCourses={likedCourses} setLikedCourses={setLikedCourses}></Card></SwiperSlide>)
 
         }
 

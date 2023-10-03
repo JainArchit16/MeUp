4e8aa41b512584316007acc56019e64b7352dfe2
Archit <jainarchit1604@gmail.com>
2023-10-03 10:40:47 +0530

    Added Slide

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const Slide = (props) => {
-    let datK=props.data;
-    const [isShown, setIsShown] = useState(false);
+  let datK = props.data;
+  const [isShown, setIsShown] = useState(false);
+
   return (
-    <div className='relative h-[400px] w-[300px]' onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
-      <img src={datK.image} className='w-full h-full opacity-90'></img>
-      <p className='z-20 p-4 font-bold absolute bottom-4 text-white text-xl'>{datK.place}</p>
-      {
-        isShown?(
-        <p className='z-20 p-4 absolute -bottom-1 text-white text-lg'>{datK.eventNumber} Events</p>
-
-        ):(<></>)
-      }
-      <p></p>
+    <div
+      className='relative h-[400px] w-[300px]'
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      <img src={datK.image} className='w-full h-full opacity-90' />
+      <div className='bg-black bg-opacity-20 absolute w-full bottom-0 p-2 bg-gradient'>
+        <div className='z-20 font-bold text-white text-xl'>{datK.place}</div>
+        <p
+          className={`z-20 text-white text-lg ${
+            isShown ? 'opacity-100' : 'hidden opacity-0'
+          } transition-all duration-1000`}
+        >
+          {datK.eventNumber} Events
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slide
+export default Slide;
